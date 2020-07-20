c56c4be2c717fb324aba4879d7d37892007b5ee2
Ryan Koval <ryan@ryankoval.com>
2020-07-20 12:38:12 -0500

    added s3 bucket searcher

---
+++ searchers/s3_buckets.go
@@ -0,0 +1,41 @@
+package searchers
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/service/s3"
+	aw "github.com/deanishe/awgo"
+	"github.com/rkoval/alfred-aws-console-services-workflow/core"
+)
+
+func SearchS3Buckets(wf *aw.Workflow, query string) error {
+	sess, cfg := core.LoadAWSConfig()
+	svc := s3.New(sess, cfg)
+
+	resp, err := svc.ListBuckets(&s3.ListBucketsInput{})
+	if err != nil {
+		wf.NewItem(err.Error()).
+			Icon(aw.IconError)
+		return err
+	}
+	log.Println("resp", resp)
+
+	for _, bucket := range resp.Buckets {
+		title := *bucket.Name
+		subtitle := "Created " + bucket.CreationDate.Format(time.UnixDate)
+
+		wf.NewItem(title).
+			Subtitle(subtitle).
+			Arg(fmt.Sprintf(
+				"https://console.aws.amazon.com/s3/buckets/%s/?region=%s&tab=overview",
+				*bucket.Name,
+				*cfg.Region,
+			)).
+			Icon(core.GetImageIcon("s3")).
+			Valid(true)
+	}
+
+	return nil
+}
--- searchers/searchers.go
+++ searchers/searchers.go
@@ -8,6 +8,8 @@ var SearchersByServiceId map[string]searcher = map[string]searcher{
 	"ec2":                           SearchEC2Instances,
 	"ec2_instances":                 SearchEC2Instances,
 	"ec2_securityGroups":            SearchEC2SecurityGroups,
+	"s3":                            SearchS3Buckets,
+	"s3_buckets":                    SearchS3Buckets,
 	"elasticbeanstalk":              SearchElasticBeanstalkEnvironments,
 	"elasticbeanstalk_environments": SearchElasticBeanstalkEnvironments,
 }
