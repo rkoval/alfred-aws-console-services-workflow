108bee5fc22c535db55c1bfdacea262a0db89ef6
Ryan Koval <ryan@ryankoval.com>
2020-07-22 02:57:51 -0500

    added comment

--- test-ci.sh
+++ test-ci.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# mock out AWS env vars in CI so that aws-sdk-go is happy
 export AWS_ACCESS_KEY_ID=AAAAAAAAAAAAAAAAAAAA
 export AWS_SECRET_ACCESS_KEY=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 export AWS_DEFAULT_REGION=us-west-2
