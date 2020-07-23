dcca1f8074541b54f92c6a4b49c84b9e2ea0cfe1
Ryan Koval <ryan@ryankoval.com>
2020-07-23 16:25:35 -0500

    fixed bash scripts

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 export alfred_workflow_bundleid="com.ryankoval.awsconsoleservices"
 export alfred_version=
 
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" == "Darwin" ] && [ -z "$TEST" ]; then
   # make this mirror where alfred stores cache/data
   if [ -d "$HOME/Library/Application Support/Alfred" ]; then
     data_dir="com.runningwithcrayons.Alfred"
@@ -12,8 +12,8 @@ if [ "$(uname)" == "Darwin" ]; then
     data_dir="com.runningwithcrayons.Alfred-3"
     cache_dir="Alfred 3"
   fi
-  export alfred_workflow_data="$HOME/Library/Caches/$data_dir/Workflow Data/$alfred_workflow_bundleid"
-  export alfred_workflow_cache="$HOME/Library/Application Support/$cache_dir/Workflow Data/$alfred_workflow_bundleid"
+  export alfred_workflow_data="$HOME/Library/Caches/$cache_dir/Workflow Data/$alfred_workflow_bundleid"
+  export alfred_workflow_cache="$HOME/Library/Application Support/$data_dir/Workflow Data/$alfred_workflow_bundleid"
 else
   # CI won't have alfred directories, so just create in repo root
   root="$(git rev-parse --show-toplevel)/.alfred"
--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+export TEST=1
 source env.sh
 go generate ./...
 UPDATE_SNAPSHOTS=true go test ./... $@
