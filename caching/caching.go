be4724bc3469d8301b84dc2fd9559078d0742bfd
Ryan Koval <ryan@ryankoval.com>
2020-07-27 01:41:42 -0500

    added better error handling of missing aws config

--- caching/caching.go
+++ caching/caching.go
@@ -1,13 +1,10 @@
 package caching
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"strconv"
-	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	aw "github.com/deanishe/awgo"
 
@@ -20,19 +17,21 @@ type Entity = generic.Type
 type EntityArrayFetcher = func(*session.Session) ([]Entity, error)
 
 func LoadEntityArrayFromCache(wf *aw.Workflow, session *session.Session, cacheName string, fetcher EntityArrayFetcher, forceFetch bool, fullQuery string) []Entity {
-	if *session.Config.Region == "" {
-		panic(aws.ErrMissingRegion)
-	}
 	// TODO optimization: not all services have sa region associated with them, so cache can be reused across regions (e.g., s3 buckets are global)
 	cacheName += "_" + *session.Config.Region
 
 	results := []Entity{}
-	var jobName = "fetch"
+	lastFetchErrPath := wf.CacheDir() + "/last-fetch-err.txt"
 	if forceFetch {
 		log.Printf("fetching from aws ...")
 		results, err := fetcher(session)
+
 		if err != nil {
+			log.Printf("fetch error occurred. writing to %s ...", lastFetchErrPath)
+			ioutil.WriteFile(lastFetchErrPath, []byte(err.Error()), 0600)
 			panic(err)
+		} else {
+			os.Remove(lastFetchErrPath)
 		}
 		log.Printf("fetched %d results from aws", len(results))
 
@@ -43,20 +42,11 @@ func LoadEntityArrayFromCache(wf *aw.Workflow, session *session.Session, cacheNa
 		return results
 	}
 
-	maxCacheAgeSeconds := 180
-	m := os.Getenv("ALFRED_AWS_CONSOLE_SERVICES_WORKFLOW_MAX_CACHE_AGE_SECONDS")
-	if m != "" {
-		converted, err := strconv.Atoi(m)
-		if err != nil {
-			panic(err)
-		}
-		if converted != 0 {
-			log.Printf("using custom max cache age of %v seconds", converted)
-			maxCacheAgeSeconds = converted
-		}
+	err := handleExpiredCache(wf, cacheName, lastFetchErrPath, fullQuery)
+	if err != nil {
+		return []Entity{}
 	}
 
-	maxCacheAge := time.Duration(maxCacheAgeSeconds) * time.Second
 	if wf.Cache.Exists(cacheName) {
 		log.Printf("using cache with key `%s` in %s ...", cacheName, wf.CacheDir())
 		if err := wf.Cache.LoadJSON(cacheName, &results); err != nil {
@@ -66,20 +56,6 @@ func LoadEntityArrayFromCache(wf *aw.Workflow, session *session.Session, cacheNa
 		log.Printf("cache with key `%s` did not exist in %s ...", cacheName, wf.CacheDir())
 		wf.NewItem("Fetching ...").
 			Icon(aw.IconInfo)
-	}
-
-	if wf.Cache.Expired(cacheName, maxCacheAge) {
-		log.Printf("cache with key `%s` was expired (older than %d seconds) in %s", cacheName, maxCacheAgeSeconds, wf.CacheDir())
-		wf.Rerun(0.4)
-		if !wf.IsRunning(jobName) {
-			cmd := exec.Command(os.Args[0], "-query="+fullQuery+"", "-fetch")
-			log.Printf("running `%s` in background as job `%s` ...", cmd, jobName)
-			if err := wf.RunInBackground(jobName, cmd); err != nil {
-				panic(err)
-			}
-		} else {
-			log.Printf("background job `%s` already running", jobName)
-		}
 	}
 
 	return results
---
+++ caching/fetch_tools.go
@@ -0,0 +1,83 @@
+package caching
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	aw "github.com/deanishe/awgo"
+	"github.com/rkoval/alfred-aws-console-services-workflow/util"
+)
+
+var jobName = "fetch"
+
+func handleExpiredCache(wf *aw.Workflow, cacheName string, lastFetchErrPath string, fullQuery string) error {
+	maxCacheAgeSeconds := 180
+	m := os.Getenv("ALFRED_AWS_CONSOLE_SERVICES_WORKFLOW_MAX_CACHE_AGE_SECONDS")
+	if m != "" {
+		converted, err := strconv.Atoi(m)
+		if err != nil {
+			panic(err)
+		}
+		if converted != 0 {
+			log.Printf("using custom max cache age of %v seconds", converted)
+			maxCacheAgeSeconds = converted
+		}
+	}
+
+	maxCacheAge := time.Duration(maxCacheAgeSeconds) * time.Second
+	if wf.Cache.Expired(cacheName, maxCacheAge) {
+		log.Printf("cache with key `%s` was expired (older than %d seconds) in %s", cacheName, maxCacheAgeSeconds, wf.CacheDir())
+		wf.Rerun(0.4)
+		if !wf.IsRunning(jobName) {
+			cmd := exec.Command(os.Args[0], "-query="+fullQuery+"", "-fetch")
+			log.Printf("running `%s` in background as job `%s` ...", cmd, jobName)
+			if err := wf.RunInBackground(jobName, cmd); err != nil {
+				panic(err)
+			}
+		} else {
+			log.Printf("background job `%s` already running", jobName)
+		}
+
+		return handleFetchErr(wf, lastFetchErrPath)
+	}
+
+	return nil
+}
+
+func handleFetchErr(wf *aw.Workflow, lastFetchErrPath string) error {
+	data, err := ioutil.ReadFile(lastFetchErrPath)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			// this file will often not exist, so don't spam logs if it doesn't
+			log.Println(err)
+		}
+		return nil
+	}
+
+	errString := string(data)
+	wf.Configure(aw.SuppressUIDs(true))
+	if strings.HasPrefix(errString, "NoCredentialProviders") {
+		util.NewURLItem(wf, "AWS credentials not configured in ~/.aws/credentials").
+			Subtitle("Press enter to open AWS docs for how to configure").
+			Arg("https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#specifying-credentials").
+			Icon(aw.IconError).
+			Valid(true)
+	} else if strings.HasPrefix(errString, "MissingRegion") {
+		util.NewURLItem(wf, "AWS default region not configured in ~/.aws/config").
+			Subtitle("Press enter to open AWS docs for how to configure").
+			Arg("https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#specifying-the-region").
+			Icon(aw.IconError).
+			Valid(true)
+	} else {
+		wf.NewItem(errString).
+			Icon(aw.IconError)
+	}
+
+	return errors.New(errString)
+}
