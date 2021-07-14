8ad70ba8e3b1a73cd45b3aabf42f0a90af72edc5
Ryan Koval <ryan@ryankoval.com>
2021-07-14 02:48:03 -0500

    fixed build maybe

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@ command -v genny >> /dev/null || go get github.com/cheekybits/genny
 command -v goimports >> /dev/null || go get golang.org/x/tools/cmd/goimports
 go generate ./...
 go run generators/gen-caching_importer/main.go
-gofmt -w caching/gen-caching.go
+goimports -w caching/gen-caching.go
