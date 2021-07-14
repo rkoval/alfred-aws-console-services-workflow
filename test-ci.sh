bbb8a6b83f79a5ac0e8a15c8f5f60d9b47ef7ce9
Ryan Koval <ryan@ryankoval.com>
2021-07-14 04:41:43 -0500

    fixed build maybe

--- test-ci.sh
+++ test-ci.sh
@@ -3,6 +3,10 @@ set -e
 ./build.sh
 
 # mock out AWS env vars in CI so that aws-sdk-go is happy
+if [ ! -f "$HOME/.aws/config" ]; then
+  mkdir -p "$HOME/.aws"
+  echo "[default]" > "$HOME/.aws/config"
+fi
 export AWS_ACCESS_KEY_ID=AAAAAAAAAAAAAAAAAAAA
 export AWS_SECRET_ACCESS_KEY=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 ./test.sh
