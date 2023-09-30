0f394aaf2fbc374653cea68f9cab2aab045d994c
Ahsan Fazal <me@ahsanfazal.com>
2023-09-30 06:14:52 +0200

    Remove region from `finalUrl`

--- util/aws_tools.go
+++ util/aws_tools.go
@@ -186,7 +186,7 @@ func GenerateLogoutLoginUrl(destinationUrl string) url.URL {
 
 	finalUrl := url.URL{
 		Scheme:   "https",
-		Host:     "us-east-1.signin.aws.amazon.com",
+		Host:     "signin.aws.amazon.com",
 		Path:     "/oauth",
 		RawQuery: url.Values(logoutUrlParams).Encode(),
 	}
