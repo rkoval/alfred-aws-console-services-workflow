e956bd8dbb07800ccd9e26e690378b4bf7f1992f
Ryan Koval <ryan@ryankoval.com>
2024-08-09 15:33:04 -0500

    lol

--- generate.sh
+++ generate.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
-which goimports
-command -v goimports >> /dev/null || go install golang.org/x/tools/cmd/goimports@v0.24.0 golang.org/x/tools/cmd/goimports
+command -v goimports >> /dev/null || go install golang.org/x/tools/cmd/goimports@v0.24.0
 go generate ./...
